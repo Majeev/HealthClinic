bfe2815878ec649bed4c91ee8ea1b7b9343e9f53
Kuba <majewski.j01@gmail.com>
2024-11-29 12:23:42 +0100

    feature: add GET endpoint for customers table

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CustomersService } from './customers.service';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  constructor(private readonly customersService: CustomersService) {}
+
+  @Get()
+  index() {
+    return this.customersService.getAllCustomers();
+  }
+}
--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Customer } from './customers.entity';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  constructor(
+    @InjectRepository(Customer)
+    private readonly customersRepository: Repository<Customer>,
+  ) {}
+
+  async getAllCustomers() {
+    return await this.customersRepository.find();
+  }
+}
